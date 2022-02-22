d980ac3f58ba5bfcdcfba50891901844c7639c6b
Randy <randypriv@gmail.com>
2022-02-22 18:30:47 +0800

    add description

--- packages/sairin-theme-minimal/src/OpenGraph.tsx
+++ packages/sairin-theme-minimal/src/OpenGraph.tsx
@@ -7,15 +7,18 @@ export function OpenGraph(props: {
   return (
     <>
       <meta property="og:type" content="artical" />
+      <meta property="og:description" content={props.post.body.substring(0, 140)} />
+      <meta property="og:title" content={props.post.title} />
+      <meta property="og:image" content={ogImage} />
+
       <meta property="article:published_time" content={props.post.created_at} />
       <meta property="article:author" content={props.post.user.login} />
+
 
       <meta name="twitter:card" content={props.post.title} />
       <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral" />
       <meta name="twitter:image" content={ogImage} />
 
-      <meta property="og:title" content={props.post.title} />
-      <meta property="og:image" content={ogImage} />
     </>
   )
 }
